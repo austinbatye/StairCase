be0ffad9dd4e3c566ec11363a52049916e439308
austinbatye <austin.batye@outlook.com>
2018-05-01 18:33:55 -0400

    comit

--- StairCase/Program.cs
+++ StairCase/Program.cs
@@ -10,6 +10,14 @@ namespace StairCase
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Input number of stairs: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                string spaces = new String(' ', (n - i));
+                string sharps = new String('#', i);
+                Console.WriteLine($"{spaces}{sharps}");
+            }
         }
     }
 }
